507baea5c7f79a3286fa8c41e851407554062b9d
nikhilreddy3 <35391154+nikhilreddy3@users.noreply.github.com>
2018-09-13 19:33:44 +0530

    Create test.c

---
+++ Lexer/test.c
@@ -0,0 +1,110 @@
+
+//single line comment - Should be ignored
+
+/* multiline
+ comment              - Should be ignored
+ */
+
+//keywords
+auto
+break
+case
+char
+const
+continue
+default
+do
+double
+else
+enum
+extern
+float
+for
+goto
+if
+inline
+int
+long
+register
+restrict
+return
+short
+signed
+sizeof
+static
+struct
+switch
+typedef
+union
+unsigned
+void
+volatile
+while
+_Bool
+_Complex
+_Imaginary
+
+//Identifiers
+integer
+pi314
+_333nikhil_reddy
+
+//Constants
+985
+3.33
+5.
+5e+16
+3.33E-345
+'nikhil'
+
+//String-Literals
+"NIKHIL REDDY ********* 16CS30038"
+"COMPILERS+ASSIGNMENT"
+
+//punctuators
+[
+++
+/
+?
+=
+,
+]
+(
+{ } . ->
+* + - ~ !
+% << >> < > <= >=
+: ; ...
+*= /= %= += -= <<=
+#
+--
+)
+&
+==
+>>=
+!=
+&=
+^
+|
+^=
+&&
+||
+|=
+
+//Mixture of tokens
+int pos=init*60.0;
+
+char c='a';
+
+while(a>n)
+{
+	a++;
+}
+
+if(k==p)
+	p++;
+else
+	k++;
+
+enum days{"mon","tues","wed","thur","fri","sun"};
+
+printf("%d",i);
